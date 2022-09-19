e1fe68fab042a7752c0c02e2e0ff1b8888f84ed8
Iqrorjon Islomov <111178357+IqrorjonCoder@users.noreply.github.com>
2022-09-19 10:54:48 +0500

    Add files via upload

--- autofiller.py
+++ autofiller.py
@@ -3,29 +3,27 @@ from flask import Flask, render_template, request
 app = Flask(__name__)
 
 
-
 @app.route('/')
 def index():
     return render_template('new.html')
 
 
-@app.route("/download" , methods=['GET', 'POST'])
+@app.route("/download", methods=['GET', 'POST'])
 def test():
-    data = {}
-    data['unit'] = str(request.form.get('comp_select'))
-    data['word'] = request.form.get('word')
-    data['speaking_word'] = request.form.get('speaking_word')
-    data['translated_text_1'] = request.form.get('translated_text_1')
-    data['example_sentence_1'] = request.form.get('example_sentence_1')
-    data['translated_text_2'] = request.form.get('translated_text_2')
-    data['example_sentence_2'] = request.form.get('example_sentence_2')
+    data = {'unit': str(request.form.get('comp_select')), 'word': request.form.get('word'),
+            'speaking_word': request.form.get('speaking_word'),
+            'translated_text_1': request.form.get('translated_text_1'),
+            'example_sentence_1': request.form.get('example_sentence_1'),
+            'translated_text_2': request.form.get('translated_text_2'),
+            'example_sentence_2': request.form.get('example_sentence_2')}
 
     from static.main import convertor
-    
-    return render_template('download.html', data=data), convertor(data['word'], data['unit'], data['word'], data['speaking_word'], data['translated_text_1'], data['example_sentence_1'], data['translated_text_2'], data['example_sentence_2'])
 
-#
+    return render_template('download.html', data=data), convertor(data['word'], data['unit'], data['word'],
+                                                                  data['speaking_word'], data['translated_text_1'],
+                                                                  data['example_sentence_1'], data['translated_text_2'],
+                                                                  data['example_sentence_2'])
 
 
-if __name__=='__main__':
+if __name__ == '__main__':
     app.run(debug=True)
--- static/main.py
+++ static/main.py
@@ -1,141 +1,234 @@
 from html2image import Html2Image
 
-def convertor(save_as, unit_name, word, speaking_word, example1_translate, example1_sentence, example2_translate, example2_sentence):
 
-    hti = Html2Image()
+def convertor(save_as, unit_name, word, speaking_word, example1_translate, example1_sentence, example2_translate,
+              example2_sentence):
+    hti = Html2Image(output_path='C:/Users/Student/AppData/IqrorjonCoder/Vs-Code/Auto-filler/autofiller/static/')
 
-    html = f"""
-    
-    <!DOCTYPE html>
-    <html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Auto Filler</title>
-        <link rel="stylesheet" href="main.css">
-    </head>
-    <body>
-    
-        <div class="head_div container">
-            <div class="headdiv_1">
-                <b>Destinition</b>
-                <i><br>Vocabulary</i>
-            </div>
-    
-            <div class="headdiv_2">
-                <b>B1</b>
-            </div>
-    
-            <div class="headdiv_3">
-                <p>{unit_name}</p>
-            </div>
+    if example2_translate == "" and example2_sentence == "":
+
+        html = f"""
+
+<div class="body">
+
+    <div class="head_div container">
+        <img src="C:/Users/Student/AppData/IqrorjonCoder/Vs-Code/Auto-filler/autofiller/static/des_name.png" alt="Destinition name" class="destinition_image">
+        <div class="headdiv_3">
+            <p>{unit_name}</p>
         </div>
+    </div>
+
+    <div class="text_name">
+        <p>{word}</p>
+    </div>
     
-        <div class="text_name">
-            <p>{word}</p>
+    <div class="text_speaking">
+        <h1>{speaking_word}</h1>
+    </div>
+
+    <div class="mano1 container">
+        <p>{example1_translate}</p>
+        <p class="mano1_misol">{example1_sentence}</p>
+    </div>
+
+</div>
+   
+        """
+        css = """ 
+.body{
+    width: 100%;
+    height: 100%;
+    background-color: white;
+    margin: none;
+}
+
+.container{
+    margin-left: 40px;
+    width: auto;
+    height: auto;
+}
+.head_div{
+    width: 100%;
+    height: 170px;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+}
+
+.destinition_image{
+    width: 500px;
+    height: 120px;
+}
+
+.headdiv_3 p{
+    font-size:50px;
+    margin-right: 80px;
+    font-family: sans-serif;
+}
+
+.text_name{
+    width: 100%;
+    height: 200px;
+    margin-top: 30px;
+    display: flex;
+    justify-content:center;
+    align-items: center;
+    font-family: sans-serif;
+}
+
+.text_name p{
+    font-size: 160px;
+}
+
+.text_speaking{
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    font-size: 25px;
+    color: #7db521;
+    font-family: sans-serif;
+}
+
+.mano1 p{
+    font-size: 35px;
+    font-family: sans-serif;
+}
+
+.mano1_misol{
+    width: 80%;
+    height: 130px;
+    margin-left: 10%;
+    font-size: 30px;
+    font-style: italic;
+    margin: none;
+}
+
+            """
+
+        hti.screenshot(html_str=html, css_str=css, save_as=f'{save_as}.jpg', size=(1700, 800))
+        # print(save_as, unit_name, word, speaking_word, example1_translate, example1_sentence, example2_translate,
+        #       example2_sentence)
+
+    else:
+        html = f"""
+<div class="body">
+
+    <div class="head_div container">
+        <img src="C:/Users/Student/AppData/IqrorjonCoder/Vs-Code/Auto-filler/autofiller/static/des_name.png" alt="Destinition name" class="destinition_image">
+        <div class="headdiv_3">
+            <p>{unit_name}</p>
         </div>
-        
-        <div class="text_speaking">
-            <h1>{speaking_word}</h1>
-        </div>
+    </div>
+
+    <div class="text_name">
+        <p>{word}</p>
+    </div>
     
-        <div class="mano1 container">
-            <p>{example1_translate}</p>
-            <p class="mano1_misol">{example1_sentence}</p>
-        </div>
-    
-        <div class="mano1 container">
-            <p>{example2_translate}</p>
-            <p class="mano1_misol">{example2_sentence}</p>
-        </div>
-    
-    </body>
-    </html>
+    <div class="text_speaking">
+        <h1>{speaking_word}</h1>
+    </div>
+
+    <div class="mano1 container">
+        <p>{example1_translate}</p>
+        <p class="mano1_misol">{example1_sentence}</p>
+    </div>
+
+    <div class="mano2 container">
+        <p>{example2_translate}</p>
+        <p class="mano2_misol">{example2_sentence}</p>
+    </div>
+
+</div>
+            """
+
+        css = """ 
+
+
+.body{
+    width: auto;
+    height: auto;
+    background-color: white;
+}
+
+.container{
+    margin-left: 40px;
+    width: auto;
+    height: auto;
+}
+.head_div{
+    width: 100%;
+    height: 170px;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+}
+
+.destinition_image{
+    width: 500px;
+    height: 120px;
+}
+
+.headdiv_3 p{
+    font-size:50px;
+    margin-right: 80px;
+    font-family: sans-serif;
+}
+
+.text_name{
+    width: 100%;
+    height: 200px;
+    margin-top: 30px;
+    display: flex;
+    justify-content:center;
+    align-items: center;
+    font-family: sans-serif;
+}
+
+.text_name p{
+    font-size: 160px;
+}
+
+.text_speaking{
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    font-size: 25px;
+    color: #7db521;
+    font-family: sans-serif;
+}
+
+.mano1 p{
+    font-size: 35px;
+    font-family: sans-serif;
+}
+
+.mano1_misol{
+    width: 80%;
+    height: 130px;
+    margin-left: 10%;
+    font-size: 30px;
+    font-style: italic;
+    margin: none;
+}
+
+.mano2 p{
+    font-size: 35px;
+    font-family: sans-serif;
+}
+.mano2_misol{
+    width: 80%;
+    height: 130px;
+    margin-left: 10%;
+    font-size: 30px;
+    font-style: italic;
+    margin: none;
+}
     
     """
 
+        hti.screenshot(html_str=html, css_str=css, save_as=f'{save_as}.jpg', size=(1700, 1000))
+        # print(save_as, unit_name, word, speaking_word, example1_translate, example1_sentence, example2_translate,
+        #       example2_sentence)
 
-    css = """ 
-
-    .container{
-        margin-left: 40px;
-    }
-    .head_div{
-        width: 100%;
-        height: 170px;
-        display: flex;
-    }
-    
-    .headdiv_1{
-        width: 400px;
-        height: 100%;
-        display: block;
-    }
-    
-    .headdiv_1 b{
-        font-size: 80px;
-        font-family:'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif;
-    }
-    
-    .headdiv_1 i{
-        font-size: 50px;
-    }
-    
-    .headdiv_2{
-        width: 65%;
-        height: 100%;
-    }
-    
-    .headdiv_2 b{
-        font-size: 160px;
-        color: rgb(3, 221, 3);
-        margin-left: 30px;
-        font-family:'Franklin Gothic Medium'
-    }
-    
-    .headdiv_3 p{
-        font-size:50px;
-    }
-    
-    .text_name{
-        width: 100%;
-        height: 200px;
-        margin-top: 30px;
-        display: flex;
-        justify-content:center;
-        align-items: center;
-        font-family: sans-serif;
-    }
-    
-    .text_name p{
-        font-size: 160px;
-    }
-    
-    .text_speaking{
-        display: flex;
-        justify-content: center;
-        align-items: center;
-        font-size: 25px;
-        color: rgb(3, 221, 3);
-    }
-    
-    .mano1 p{
-        font-size: 35px;
-    }
-    
-    .mano1_misol{
-        width: 80%;
-        height: 130px;
-        margin-left: 10%;
-        font-size: 30px;
-        font-style: italic;
-        margin: none;
-    }
-    
-    """
-
-    hti.screenshot(html_str=html, css_str=css, save_as=f'{save_as}.jpg')
-
-#
-# convertor('Unit 3', "board ggame", "/bɔːd ɡeɪm/", "1. stol ustida o'ynaluvchi o'yin", "This is a great board game to play with kids - Bu bolalar bilan o'ynash uchun ajoyib stol o'yini", "2. stol ustida o'ynaluvchi o'yin", "me to play with kids - Bu bolalar bilan o'ynash uchun ajoyib st", "1")
+# convertor("sava_as", 'Unit 2', 'hello', 'he:ll', '1. salom', 'this is first example', '4352',
+#           'fghsdgh')
